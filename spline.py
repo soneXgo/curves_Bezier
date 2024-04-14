1118b9cf439f8c9c59a69fd4260a3711c35229ba
xeniaTa <kseniatarsenko1@gmail.com>
2024-04-14 15:04:15 +0300

    fix spline B

--- spline.py
+++ spline.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pygame
 
 def bezier_curve(points, num_points = 10000):
     curve = []
@@ -14,90 +15,92 @@ def bezier_curve(points, num_points = 10000):
         curve.append([x, y])
     return curve
 
-# def bezier_curve(control_points, num_points=100):
-#     n = len(control_points)
-#     t = np.linspace(0, 1, num_points)
-#     curve_points = []
-#     for i in range(num_points):
-#         x = 0
-#         y = 0
-#         for j in range(n):
-#             blend = np.math.factorial(n-1) / (np.math.factorial(j) * np.math.factorial(n-1-j)) * ((1-t[i])**(n-1-j)) * (t[i]**j)
-#             x += blend * control_points[j][0]
-#             y += blend * control_points[j][1]
-#         curve_points.append([x, y])
-#     return curve_points
-
 def get_curve_control_points(knots):
     n = len(knots) - 1
-    if n == 1:  #Special case: Bezier curve should be a straight line.
-        first_control_points = [(2 * knots[0][0] + knots[1][0]) / 3, (2 * knots[0][1] + knots[1][1]) / 3]
-        second_control_points = [2 * first_control_points[0] - knots[0][0], 2 * first_control_points[1] - knots[0][1]]
-        return [first_control_points], [second_control_points]
+    if n == 1:  # Bezier curve should be a straight line
+        # 3P1 = 2K0 + K3
+        first_ctrl_points = [(2 * knots[0][0] + knots[1][0]) / 3, (2 * knots[0][1] + knots[1][1]) / 3]
+        # P2 = 2P1 – K0
+        second_ctrl_points = [2 * first_ctrl_points[0] - knots[0][0], 2 * first_ctrl_points[1] - knots[0][1]]
+        return [first_ctrl_points], [second_ctrl_points]
 
-    # Calculate first Bezier control points
-	# Right hand side vector
+    # calculate first Bezier control points
+	# right hand side vector
     rhs = np.zeros(n)
     x = np.zeros(n)
     y = np.zeros(n)
 
-    # Set right hand side X values
+    # set right hand side X values
     for i in range(1, n-1):
         rhs[i] = 4 * knots[i][0] + 2 * knots[i + 1][0]
     rhs[0] = knots[0][0] + 2 * knots[1][0]
     rhs[n-1] = (8 * knots[n-1][0] + knots[n][0]) / 2.0
+    # get first control points X-values
     x = get_first_control_points(rhs)
 
-    # Set right hand side Y values
+    # set right hand side Y values
     for i in range(1, n-1):
         rhs[i] = 4 * knots[i][1] + 2 * knots[i + 1][1]
     rhs[0] = knots[0][1] + 2 * knots[1][1]
     rhs[n-1] = (8 * knots[n-1][1] + knots[n][1]) / 2.0
+    # get first control points Y-values
     y = get_first_control_points(rhs)
 
-    first_control_points = []
-    second_control_points = []
+    # fill output arrays
+    first_ctrl_points = []
+    second_ctrl_points = []
     for i in range(n):
-        first_control_points.append([x[i], y[i]])
+        # first control point
+        first_ctrl_points.append([x[i], y[i]])
+        # second control point
         if (i < n - 1):
-            second_control_points.append([2 * knots[i+1][0] - x[i+1], 2 * knots[i+1][1] - y[i+1]])
+            second_ctrl_points.append([2 * knots[i+1][0] - x[i+1], 2 * knots[i+1][1] - y[i+1]])
         else:
-            second_control_points.append([(knots[n][0] + x[n-1]) / 2, (knots[n][1] + y[n-1]) / 2])
+            second_ctrl_points.append([(knots[n][0] + x[n-1]) / 2, (knots[n][1] + y[n-1]) / 2])
             
-    return first_control_points, second_control_points
+    return first_ctrl_points, second_ctrl_points
 
+# Solves a tridiagonal system for one of coordinates (x or y)
+# of first Bezier control points.
 def get_first_control_points(rhs):
     n = len(rhs)
-    x = np.zeros(n) # Solution vector
-    tmp = np.zeros(n) # Temp workspace
+    x = np.zeros(n) # solution vector
+    tmp = np.zeros(n) # temp workspace
 
     b = 2.0
     x[0] = rhs[0] / b
-    for i in range(1, n): # Decomposition and forward substitution
+    # decomposition and forward substitution
+    for i in range(1, n): 
         tmp[i] = 1 / b
         b = (4.0 if i < n - 1 else 3.5) - tmp[i]
         x[i] = (rhs[i] - x[i - 1]) / b
+    # backsubstitution
     for i in range(1, n):
-        x[n - i - 1] -= tmp[n - i] * x[n - i]  # Backsubstitution
+        x[n - i - 1] -= tmp[n - i] * x[n - i]  
 
     return x
 
+def get_bezier_spline_points(knots):
+    first_ctrl_points, second_ctrl_points = get_curve_control_points(knots)
+    ctrl_points = []
+    spline_points = []
+    for i in range(0, len(knots)-1):
+        curve_points = [knots[i], first_ctrl_points[i], second_ctrl_points[i], knots[i+1]]
+        spline_points += bezier_curve(curve_points)
+        ctrl_points += curve_points
+    return spline_points, ctrl_points
+
 # Пример данных точек
-knots = [[0, 0], [2, 1], [4, 4], [7, 7], [10,4], [12,1], [14, 0]]
-# knots = [[0, 0], [1, 3], [2, 1],[3,2]]
+#knots = [[0, 0], [2, 1], [4, 4], [7, 7], [10,4], [12,1], [14, 0]]
+#knots = [[0, 1.5], [2, 3], [4, 1],[6,2]]
+knots = [[3, 3], [1, 5], [2,6],[2.8, 5.5], [3, 5.2],[3.2, 5.5],[4,6], [5,5],[3,3]]
+#knots = [[1, 1.5], [2, 1],[3,2]]
 
-first_control_points, second_control_points = get_curve_control_points(knots)
-c_points =[]
-cur_points=[]
-for i in range(0, len(knots)-1):
-    control_points = [knots[i],first_control_points[i], second_control_points[i], knots[i+1]]
-    cur_points += bezier_curve(control_points)
-    c_points +=control_points
-
+spline_points, ctrl_points = get_bezier_spline_points(knots)
 
 plt.figure()
-plt.plot([point[0] for point in cur_points], [point[1] for point in cur_points], 'b-', label='Bezier Curve')
-plt.plot([point[0] for point in c_points], [point[1] for point in c_points], 'ro-', label='Control Points')
+plt.plot([point[0] for point in spline_points], [point[1] for point in spline_points], 'b-')
+plt.plot([point[0] for point in ctrl_points], [point[1] for point in ctrl_points], 'ro-')
 
 plt.xlabel('X')
 plt.ylabel('Y')
@@ -105,4 +108,3 @@ plt.title('Bezier Spline')
 plt.legend()
 plt.grid()
 plt.show()
-
